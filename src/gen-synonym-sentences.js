6e168395afd443b858aa132df8b3fef3cdb89d61
learnuidev <learnuidev@gmail.com>
2025-01-14 21:04:38 -0500

    added gen-synonym-sentences

---
+++ src/gen-synonym-sentences.js
@@ -0,0 +1,25 @@
+const { chatV2 } = require("./utils/chat-v2");
+
+async function generateSynonymSentences({ content, openai, model }) {
+  const resp = await chatV2({
+    openai,
+    model,
+    prompt: `
+You are a language expert, given the content, please generate 6 sentences that conveys the same meaning, 
+the input for each sentences should be different than the content while preserving the original meaning
+
+
+Please provide in stringified JSON format like so:
+[
+  {"input": "", "roman": "...", "en": "...", "..."}]
+    
+    `,
+    content,
+  });
+
+  return resp;
+}
+
+module.exports = {
+  generateSynonymSentences,
+};
