7a439971eb421bc166feea82bac16c61f5f5b357
learnuidev <learnuidev@gmail.com>
2025-02-02 11:16:05 -0500

    fix casual translate bug

--- __tests/casual-translate.test.js
+++ __tests/casual-translate.test.js
@@ -1,9 +1,21 @@
 const { mandarinoClient } = require("./test.client");
 
+// mandarinoClient
+//   .casualTranslate({
+//     // content: "How do you learn French",
+//     content:
+//       "ces êtres magiques sont souvent des jeunes filles. elles sont capables de se transformer en phoque et leur chant est également magique c'est leur manteau qui leur donne leur poroir, si elles en sont privées, elles sont en grand danger",
+//     sourceLang: "fr-FR",
+//     targetLang: "en",
+//   })
+//   .then((resp) => {
+//     console.log("RESP", resp);
+//   });
 mandarinoClient
   .casualTranslate({
-    content: "How do you learn French",
-    sourceLang: "en-US",
+    // content: "How do you learn French",
+    content: "i love you",
+    sourceLang: "en",
     targetLang: "es-ES",
   })
   .then((resp) => {
--- src/casual-translate.js
+++ src/casual-translate.js
@@ -17,12 +17,19 @@ You are a language expert, given the content please provide casual spoken transl
 Please provide in stringified JSON format like so:
 {"input": "", "roman": "...", "output": "..."}
 
-For example for: when i was a kid, i loved drawing, reading and singing
+For example for source lang of en and target lang of zh-CN, or zh for: when i was a kid, i loved drawing, reading and singing
 
 if should return
 {"input": "when i was a kid, i loved drawing, reading and singing",
 "output": "小时候，我特喜欢画画、看书和唱歌。",
 "roman": "xiǎoshíhou, wǒ tè xǐhuan huàhuà, kànshū hé chànggē."}
+
+For example for source lang of fr-FR and target lang of en for: ces êtres magiques sont souvent des jeunes filles. elles sont capables de se transformer en phoque et leur chant est également magique c'est leur manteau qui leur donne leur poroir, si elles en sont privées, elles sont en grand danger
+
+it should return
+{"input": "ces êtres magiques sont souvent des jeunes filles. elles sont capables de se transformer en phoque et leur chant est également magique c'est leur manteau qui leur donne leur poroir, si elles en sont privées, elles sont en grand danger",
+"output": "these magical beings are often young girls. they are capable of transforming into a seal and their song is also magical it is their coat which gives them their poroir, if they are deprived of it, they are in great danger",
+"roman": null}
 `,
     content,
   });
