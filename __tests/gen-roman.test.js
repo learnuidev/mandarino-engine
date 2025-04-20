4760d1140ad591268aa5dc8a84985c00b9c3cd66
learnuidev <learnuidev@gmail.com>
2025-04-19 23:06:57 -0400

    wip

--- __tests/gen-roman.test.js
+++ __tests/gen-roman.test.js
@@ -1,12 +1,21 @@
 const { mandarinoClient } = require("./test.client");
 
+// mandarinoClient
+//   .genRoman({
+//     content: "पहला नशा, पहला खुमार",
+//     lang: "zh",
+//   })
+//   .then((sentences) => {
+//     console.log("synonyms", JSON.stringify(sentences, null, 4));
+//   });
+
+// pahalaa nashaa, pahalaa khumaar
+
 mandarinoClient
   .genRoman({
-    content: "पहला नशा, पहला खुमार",
+    content: "想要更上一层楼的话 那你得努力挣扎",
     lang: "zh",
   })
   .then((sentences) => {
     console.log("synonyms", JSON.stringify(sentences, null, 4));
   });
-
-// pahalaa nashaa, pahalaa khumaar
