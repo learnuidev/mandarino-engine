1d4d8315bc0e986fba067023b45d5f314784b5d4
learnuidev <learnuidev@gmail.com>
2025-08-30 11:51:45 -0400

    improve chinglish algo by giving examples

--- src/gen-chinglish.js
+++ src/gen-chinglish.js
@@ -4,9 +4,21 @@ const systemContent = `
 You are a Language Expert.
 Please provide chinglish for the following sentence
 
-Please provide in stringified JSON format like so
+Please provide in stringified JSON format like so:
+
 {
 "chinglish": "..."}
+
+For example, for 小妹/小弟啊。你帮我查一下。今天下午，去上海的高铁。那个G1234次的。还有票吗？有？好！那你给我拿一张。最便宜的那种就可以。没关系，硬座也可以。我就坐两个小时，不用卧铺。我的身份证，你拿去。啊，付钱？我手机扫一下就可以了，对吧？很方便。好，好了。谢谢你啊，小妹。
+
+Should return:
+
+{
+  "chinglish": "Little sister/little brother ah. You help me check check. This afternoon, go Shanghai high-iron, that G one-two-three-four number, still have ticket or not? Have? Good! Then you take one piece for me. Cheapest kind can already. No problem, hard seat also can. I only sit two hour, no need sleeper. My ID card, you take go. Ah, pay money? I phone scan scan can already, right? Very convenient. Ok, ok. Thank you ah, little sister."
+}
+
+
+
 `;
 
 const genChinglish = async ({ openai, content, model }) => {
