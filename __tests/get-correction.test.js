568057131fd2180770740e385c79ffec8ee88563
learnuidev <learnuidev@gmail.com>
2025-09-11 15:11:44 -0400

    wip

--- __tests/get-correction.test.js
+++ __tests/get-correction.test.js
@@ -65,6 +65,51 @@ const frenchExample1 = {
     targetLang: "fr",
   },
 };
-mandarinoClient.getCorrection(frenchExample1.input).then((resp) => {
+
+const chineseExample1 = {
+  input: {
+    content:
+      "今天，哟要给大家分享我的中文学习故事。我要分享我如何学习中文。我的中文学习方法。好吧，那我们开始吧。在二零二三年我开始学中文！为什么呢？因为我感兴趣AI。我要学习AI by learning chinese. I basically wanted to apply chinese language into nlp",
+    sourceLang: "en",
+    targetLang: "zh",
+  },
+  output: {
+    content:
+      "今天，哟要给大家分享我的中文学习故事。我要分享我如何学习中文。我的中文学习方法。好吧，那我们开始吧。在二零二三年我开始学中文！为什么呢？因为我感兴趣AI。我要学习AI by learning chinese. I basically wanted to apply chinese language into nlp",
+    corrected:
+      "今天，我要给大家分享我的中文学习故事。我要分享我是如何学习中文的，以及我的中文学习方法。好吧，那我们开始吧。在二〇二三年我开始学中文！为什么呢？因为我对人工智能感兴趣。我想通过学中文来学习AI。我基本上想把中文应用到自然语言处理中。",
+    details: [
+      { original: "哟", correction: "我", startIndex: 3, endIndex: 4 },
+      {
+        original: "我要分享我如何学习中文。我的中文学习方法。",
+        correction: "我要分享我是如何学习中文的，以及我的中文学习方法。",
+        startIndex: 19,
+        endIndex: 40,
+      },
+      {
+        original: "我感兴趣AI",
+        correction: "我对人工智能感兴趣",
+        startIndex: 70,
+        endIndex: 76,
+      },
+      {
+        original: "我要学习AI by learning chinese.",
+        correction: "我想通过学中文来学习AI。",
+        startIndex: 77,
+        endIndex: 104,
+      },
+      {
+        original: "I basically wanted to apply chinese language into nlp",
+        correction: "我基本上想把中文应用到自然语言处理中。",
+        startIndex: 105,
+        endIndex: 158,
+      },
+    ],
+    sourceLang: "en",
+    targetLang: "zh",
+  },
+};
+
+mandarinoClient.getCorrection(chineseExample1.input).then((resp) => {
   console.log("RESP", resp);
 });
